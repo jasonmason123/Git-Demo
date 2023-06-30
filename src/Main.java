57710816577fe13ebbc02dffaa55c9a807d091f6
jasonmason123 <buiminhson940@gmail.com>
2023-06-30 19:59:05 +0700

    third commit

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,6 @@ public class Main {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is my demo for Git");
-        System.out.println("Something new!");
-        System.out.println("Hello");
     }
     
 }
